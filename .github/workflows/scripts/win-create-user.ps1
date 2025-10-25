1380da2f6a2a6c99e608a252f84fcce37949394a
hayongaixi <129855073+hayongaixi@users.noreply.github.com>
2025-10-25 17:32:48 +0800

    Update win-create-user.ps1

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -5,4 +5,4 @@ New-LocalUser -Name "administrator" -Password $securePass -AccountNeverExpires
 Add-LocalGroupMember -Group "Administrators" -Member "administrator"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "administrator"
 echo "RDP_CREDS=User: vum | Password: Pass@Word1" >> $env:GITHUB_ENV
-if (-not (Get-LocalUser -Name "vum")) { throw "User creation failed" }
+if (-not (Get-LocalUser -Name "administrator")) { throw "User creation failed" }
