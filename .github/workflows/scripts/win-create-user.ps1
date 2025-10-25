7882d09f2b81ada850b347b31507cd67f5838f1d
hayongaixi <129855073+hayongaixi@users.noreply.github.com>
2025-10-25 17:32:24 +0800

    Update win-create-user.ps1

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -1,20 +1,8 @@
 # Create RDP user with strong password
 Add-Type -AssemblyName System.Security
-$charSet = @{
-  Upper   = [char[]](65..90)
-  Lower   = [char[]](97..122)
-  Number  = [char[]](48..57)
-  Special = ([char[]](33..47) + [char[]](58..64) + [char[]](91..96) + [char[]](123..126))
-}
-$rawPassword = @()
-$rawPassword += $charSet.Upper | Get-Random -Count 4
-$rawPassword += $charSet.Lower | Get-Random -Count 4
-$rawPassword += $charSet.Number | Get-Random -Count 4
-$rawPassword += $charSet.Special | Get-Random -Count 4
-$password = -join ($rawPassword | Sort-Object { Get-Random })
-$securePass = ConvertTo-SecureString $password -AsPlainText -Force
-New-LocalUser -Name "vum" -Password $securePass -AccountNeverExpires
-Add-LocalGroupMember -Group "Administrators" -Member "vum"
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "vum"
-echo "RDP_CREDS=User: vum | Password: $password" >> $env:GITHUB_ENV
+$securePass = ConvertTo-SecureString Pass@Word1 -AsPlainText -Force
+New-LocalUser -Name "administrator" -Password $securePass -AccountNeverExpires
+Add-LocalGroupMember -Group "Administrators" -Member "administrator"
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member "administrator"
+echo "RDP_CREDS=User: vum | Password: Pass@Word1" >> $env:GITHUB_ENV
 if (-not (Get-LocalUser -Name "vum")) { throw "User creation failed" }
